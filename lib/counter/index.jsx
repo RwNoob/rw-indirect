ce64af1a1c86f22f8a48764a336e4d3b68af0e9e
Rw <2306483837@qq.com>
2024-11-16 18:32:15 +0800

    add

---
+++ lib/counter/index.jsx
@@ -0,0 +1,2 @@
+import { App } from './src/App';
+render(<App />, document.body);
---
+++ lib/counter/src/app.jsx
@@ -0,0 +1,7 @@
+export function App() {
+    return () => (
+        <div id='app'>
+            <h1>Rw App</h1>
+        </div>
+    );
+}
